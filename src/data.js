19e7559cd11abebccd8bea313d1e91e7f6186d40
avivirk <65970239+avivirk@users.noreply.github.com>
2020-05-26 22:21:59 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -142,29 +142,65 @@ export const feeds = [
 
 export const profile = {
     id: 1,
-    username: "",
-    profilePhoto: "",
-    firstName: "",
-    lastName: "",
-    profileBio: "",
+    username:"virk_92",
+    profilePhoto:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/67264038_399117230725454_1353808824531681280_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_ohc=gkL2DzgBsrUAX8ymm9h&oh=2e764d3f70070034429d53aafabebe60&oe=5EF50229",
+    name:"Avi_Virk",
+    profileBio:"BramptonðŸ",
     highlights: [
-        {
-            id: 1,
-            photo: "",
-            name: "",
-        },
+      {
+        id: 1,
+        photo:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/s150x150/90204214_1637984223015647_6235722418793609146_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_ohc=QDbvi27hhPYAX9t-r0S&oh=6d4ca7bf232e3631063b2f38edb16feb&oe=5EF6F74F",
+        name:"GymLife"
+      },
+      {
+        id: 2,
+        photo:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/s150x150/91164958_105168967710751_8283672901876769004_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_ohc=6ilculfx39YAX-3CaTs&oh=8fa05396fb153aff8166c319bcb6f312&oe=5EF4A921",
+        name:"whistler"
+      }
     ],
     metadata: {
-        totalPost: "",
-        totalFollowers: "",
-        totalFollow: "",
-    },
+        totalposts: 266,
+        totalfollowers: 1062,
+        totalfollowing: 434
+      },
     posts: [
-        {
-            id: "",
-            photoUrl: "",
-        },
-    ],
-};
-
+      {
+        id:"1",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/98328351_949350918830262_7561075598356351495_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=IKDMnKWPC6MAX-qqoj6&oh=dc763564828ae6ad954aa40e6efce2e3&oe=5EF61CEB"
+      },
+      {
+        id:"2",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/95324895_281472149558076_4492383870266727870_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=aHrx5HbkDhEAX_66cHd&oh=c2ed26145b9d05c09d8c6c793bcc52d1&oe=5EF6440D"
+      },
+      {
+        id:"3",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/31571515_332687813925105_8439583888799432704_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=D6ljd6pGNxAAX8SUNhx&oh=47b96e85c4598468f5b71306dc2ff6df&oe=5EF54F3B"
+      },
+      {
+        id:"4",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.123.1440.1440a/s640x640/64435332_2369776019947782_4209142115837711766_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=fViT-jvxPRkAX_mm0Fa&oh=afda79c23a21a46574d045c7c6aedaa4&oe=5EF7CD53"
+      },
+      {
+        id:"5",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/22638643_150252359044448_1885854310709854208_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=BnQI1fRg0M4AX-eQdwd&oh=5177508838e0535160e93e033e2f0440&oe=5EF7B0E6"
+      },
+      {
+        id:"6",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/e35/s240x240/22860699_158039638127332_4059792298668457984_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=V5kLJXLsgC8AX8rLguy&oh=b2d731c4bffcf6a32de9dfdf74e5f139&oe=5EF4C396"
+      },
+      {
+        id:"7",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/e35/c107.0.865.865a/s480x480/15259040_383365408721527_926336115449790464_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=haAwfWBFCQkAX8l5biG&oh=7f17cb2035985524b48e46bb04413c03&oe=5EF54BBC"
+      },
+      {
+        id:"8",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.47.803.803a/s480x480/20838502_477206145983262_2736377673495347200_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=N3_k_ILR26AAX8t4rZm&oh=b8115650a5203033e9ade7cecda095e5&oe=5EF7BE17"
+      },
+      {
+        id:"9",
+        photoUrl:"https://instagram.fluh2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c1.0.1078.1078a/s640x640/21042059_678710092314581_347814049427226624_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=YKFWjv8d8fkAX8ZdXkI&oh=6eb401801535b07a5e0eebe3efd51adb&oe=5EF789E6"
+      },
+    ]
+  }
 export default { stories, feeds, profile };
+  
