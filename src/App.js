8eebaf00a0384455798b55b03b960526430bfc21
avivirk <65970239+avivirk@users.noreply.github.com>
2020-05-26 22:13:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './feed.css';
 import Header from './header.js';
 import Footer from './footer';
 import Stories from './stories.js';
-import Feed from './feed.js'
+import Feed from './feed.js';
+import Profile from './Profile.js';
 
 function App() {
   return (
     <div className="main-container" >
       <Header />
-      {/* <Stories /> */}
-      <Feed />
-      {/* insert profile */}
-      <Footer />
+      {/*<Stories /> */}
+      {/*<Feed /> */}
+      <Profile />
+      <Footer /> 
     </div>
   );
 }
